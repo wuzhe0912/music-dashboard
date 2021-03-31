a7845d426df4a769aa0cef6f0d343dbf290a9ff7
Pitt Wu <kgb00128@gmail.com>
2021-03-31 21:32:59 +0800

    fixed：change folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// import NavBar from 'components/NavBar';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'constant/GlobalStyle';
+import { theme } from 'constant/Theme';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {/* <Router></Router> */}
+    </ThemeProvider>
+  );
 }
 
 export default App;
