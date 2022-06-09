05c1a69ffa563b495ef965023cdc4850549bccd3
Pitt Wu <kgb00128@gmail.com>
2022-06-09 09:40:40 +0800

    delete old

--- src/constant/BreakPoint.js
+++
@@ -1,3 +0,0 @@
-export const MEDIA_QUERY_SM = '@media screen and (min-width: 480px)';
-export const MEDIA_QUERY_MD = '@media screen and (min-width: 768px)';
-export const MEDIA_QUERY_LG = '@media screen and (min-width: 1200px)';
